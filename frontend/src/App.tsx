524cbd17b34849ad980e6cefe8154b7012c7f486
Mostafa Saeed <https://github.com/Desha-Saeed>
2022-11-05 21:12:17 +0200

    cleaned up starter app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Header, Footer } from './components';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <h2>Hello to my pro shop</h2>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
